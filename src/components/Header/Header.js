f3b707d14a127ca611cd27c9c6ec1beb6f8f8362
Shakil Hossain <shakilhossainp.cse@gmail.com>
2021-09-24 14:28:48 +0600

    emazone header desin section complate

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Header.css';
+import Logo from '../../images/logo.png'
+export default function Header() {
+    return (
+        <div>
+            <div className="img-container">
+                <img src={Logo} alt="" />
+            </div>
+            <nav>
+                <div className="cat-item">
+                    <a href="">Home</a>
+                    <a href="">Product</a>
+                    <a href="">About</a>
+                    <a href="">Cart</a>
+                </div>
+            </nav>
+        </div>
+    )
+}
